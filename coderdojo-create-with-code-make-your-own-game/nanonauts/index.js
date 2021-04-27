ea4d1dda288ee6b25b45d18b3f5d41e3d88b9fd9
Quintin Henn <quintin.henn@gmail.com>
2021-04-27 09:39:25 +0200

    Gravity for the Nanonaut

--- coderdojo-create-with-code-make-your-own-game/nanonauts/index.js
+++ coderdojo-create-with-code-make-your-own-game/nanonauts/index.js
@@ -1,3 +1,4 @@
+import background from './background.png';
 import nanonaut from './nanonaut.png';
 
 // CONSTANTS
@@ -5,6 +6,7 @@ let CANVAS_WIDTH = 800;
 let CANVAS_HEIGHT = 600;
 let NANONAUT_WIDTH = 181;
 let NANONAUT_HEIGHT = 229;
+let NANONAUT_Y_ACCELERATION = 1;
 let GROUND_Y = 540;
 
 // SETUP
@@ -14,11 +16,15 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 document.body.appendChild(canvas);
 
+let backgroundImage = new Image();
+backgroundImage.src = background;
+
 let nanonautImage = new Image();
 nanonautImage.src = nanonaut;
 
 let nanonautX = 50;
 let nanonautY = 40;
+let nanonautYSpeed = 0;
 
 window.addEventListener('load', start);
 
@@ -35,11 +41,30 @@ function mainLoop() {
 
 // PLAYER INPUT
 // UPDATING
-function update() { }
+function update() {
+  // Update Nanonaut.
+  nanonautY = nanonautY + nanonautYSpeed;
+  nanonautYSpeed = nanonautYSpeed + NANONAUT_Y_ACCELERATION;
+  if (nanonautY > (GROUND_Y - NANONAUT_HEIGHT)) {
+    nanonautY = GROUND_Y - NANONAUT_HEIGHT;
+    nanonautYSpeed = 0;
+  }
+}
 
 // DRAWING
 function draw() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  // Draw the sky.
+  ctx.fillStyle = 'LightSkyBlue';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, GROUND_Y - 40);
+
+  // Draw the background.
+  ctx.drawImage(backgroundImage, 0, -210);
+
+  // Draw the ground.
+  ctx.fillStyle = 'ForestGreen';
+  ctx.fillRect(0, GROUND_Y - 40, CANVAS_WIDTH, CANVAS_HEIGHT - GROUND_Y + 40);
 
   ctx.drawImage(nanonautImage, nanonautX, nanonautY);
 }
